ddbd10523fff867f92da3c303ddee705bc1b26fb
pavan-3000 <pavansingh3000@gmail.com>
2024-03-22 19:33:55 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name= "classification",
+    version="0.0.0.0",
+    author_name="pavan",
+    author_email="pavansingh3000@gamil.com",
+    packages=find_packages()
+)
